99f339f7d55266c091a197a6db019b5886b34456
BrandonJung <Brandonjung111@gmail.com>
2025-03-28 00:12:27 -0700

    Initial commit

---
+++ src/routes/auth.mjs
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import { mockUsers } from "../utils/constants.mjs";
+
+const router = Router();
+
+router.get("/", (request, response) => {
+  console.log(request.headers.cookies);
+  console.log(request.cookies);
+  // assumming cookie is { hello: world }
+  if (request.cookies.hello && request.cookies.hello === "world") {
+    return response.send([{ id: 2 }, { id: 3 }]);
+  }
+  return response
+    .status(403)
+    .send({ message: "Sorry, you need the correct cookie" });
+});
+
+router.post("/", (request, response) => {
+  // Validate request body first
+  const {
+    body: { username, password },
+  } = request;
+  const findUser = mockUsers.find((user) => user.username === username);
+  if (!findUser || findUser.password !== password) {
+    return response.status(401).send({ message: "Bad credentials" });
+  }
+
+  request.session.user = findUser;
+  return response.status(200).send(findUser);
+});
+
+router.get("/status", (request, response) => {
+  request.sessionStore.get(request.session.id, (err, session) => {
+    console.log(session);
+  });
+  return request.session.user
+    ? response.status(200).send(request.session.user)
+    : response.status(401).send({ message: "Not Authenticated" });
+});
+
+export default router;
---
+++ src/routes/users.mjs
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import { checkSchema, query, validationResult } from "express-validator";
+import { mockUsers } from "../utils/constants.mjs";
+import { createUserValidationSchema } from "../utils/validationSchemas.mjs";
+import { createUserHandler, getUserByIdHandler } from "../handlers/users.mjs";
+
+const router = Router();
+
+router.get("/api/users/:id", getUserByIdHandler);
+
+router.get("/", query("filter").isNumeric(), (request, response) => {
+  const result = validationResult(request);
+  console.log(result);
+
+  const {
+    query: { filter, value },
+  } = request;
+
+  if (filter && value) {
+    return response.send(mockUsers.filter(user > user[filter].includes(value)));
+  }
+  return response.send(mockUsers);
+});
+
+router.post(
+  "/create",
+  checkSchema(createUserValidationSchema),
+  createUserHandler
+);
+
+export default router;
