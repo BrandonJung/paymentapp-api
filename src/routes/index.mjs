7501203a531a6dfcd9708ec0568e86c118227177
BrandonJung <Brandonjung111@gmail.com>
2025-04-09 05:26:20 -0700

    Added bunch of routes

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -1,11 +1,19 @@
 import { Router } from "express";
+
 import usersRouter from "./users.mjs";
 import customersRouter from "./customers.mjs";
+import servicesRouter from "./services.mjs";
+import jobsRouter from "./jobs.mjs";
+import reportsRouter from "./reports.mjs";
+
 import { verifyCredentials } from "../utils/middlewares.mjs";
 
 const router = Router();
 
 router.use("/api/user", usersRouter);
 router.use("/api/customer", verifyCredentials, customersRouter);
+router.use("/api/service", verifyCredentials, servicesRouter);
+router.use("/api/job", verifyCredentials, jobsRouter);
+router.use("/api/report", verifyCredentials, reportsRouter);
 
 export default router;
